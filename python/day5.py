e0ddba4c3ed16887a7cdf7955dd7db2be848950a
Stephanie Frantz <stephlage@gmail.com>
2024-12-16 16:01:12 -0500

    ugh day 6 sucks

--- python/day5.py
+++ python/day5.py
@@ -1,21 +1,21 @@
-# day5.py
+# # day5.py
 
-from utils.utils import *
-from daily.day5a import *
-# from daily.day5b import *
-
-
-@time_it
-def main():
-    """Reads the input, processes the data, and prints the results."""
-    input = read_integer_lists("../inputs/5-input.txt")
-
-    solution_a = thinga(input)
-    solution_b = thingb(input)
-
-    print_solution_box("5a", solution_a)
-    print_solution_box("5b", solution_b)
+# from utils.utils import *
+# from daily.day5a import *
+# # from daily.day5b import *
 
 
-if __name__ == "__main__":
-    main()
+# @time_it
+# def main():
+#     """Reads the input, processes the data, and prints the results."""
+#     input = read_integer_lists("../inputs/5-input.txt")
+
+#     solution_a = thinga(input)
+#     solution_b = thingb(input)
+
+#     print_solution_box("5a", solution_a)
+#     print_solution_box("5b", solution_b)
+
+
+# if __name__ == "__main__":
+#     main()
--- python/day6.py
+++ python/day6.py
@@ -8,13 +8,13 @@ from daily.day6a import *
 @time_it
 def main():
     """Reads the input, processes the data, and prints the results."""
-    input = read_integer_lists("../inputs/6-input.txt")
+    input = read_file_as_lines("../inputs/6-input.txt")
 
-    solution_a = thinga(input)
-    solution_b = thingb(input)
+    solution_a = simulate_guard_path(input)
+    # solution_b = thingb(input)
 
     print_solution_box("6a", solution_a)
-    print_solution_box("6b", solution_b)
+    # print_solution_box("6b", solution_b)
 
 if __name__ == "__main__":
     main()
